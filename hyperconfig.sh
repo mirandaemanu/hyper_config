15189470903e6e650bb8b1563dd29bb2131a01a0
= <emanuelcascais@gmail.com>
2024-05-02 15:52:20 -0300

    hyper script update

--- hyperconfig.sh
+++ hyperconfig.sh
@@ -31,8 +31,7 @@ fix_jump_connection() {
     jump="jump1.pro1.eigbox.com"
     echo -e "\nTestando a conexão com o JUMP.."
     if  ! ping -c 4 "$jump" > /dev/null 2>&1  ; then
-        echo "Digite a senha do seu usuário (OBS: por questões de segurança, a senha não irá aparecer no terminal)"
-        sudo sed -i "/# generateHosts = false/a 10.25.73.241   jump1.pro1.eigbox.com\n 10.25.73.242   jump2.pro1.eigbox.com\n 185.199.108.133   raw.githubusercontent.com" /etc/hosts
+        sed -i "/# generateHosts = false/a 10.25.73.241   jump1.pro1.eigbox.com\n10.25.73.242   jump2.pro1.eigbox.com\n185.199.108.133   raw.githubusercontent.com" /etc/hosts
     fi
     echo -e "${cor_verde}Conexão bem sucedida ${reset}"
 }
