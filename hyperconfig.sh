8ac8b1043dc65643455a76283bd239dc388c63dc
= <emanuelcascais@gmail.com>
2024-05-02 17:31:18 -0300

    hyper update config

--- hyperconfig.sh
+++ hyperconfig.sh
@@ -14,7 +14,7 @@ echo -e "Script desenvolvido por Emanuel Cascais\n"
 
 check_user() {
     if [ ! -d "/mnt/c/Users/$usuario" ]; then
-        echo -e "O usuário informado não existe. \nPor favor, execute o script novamente e informe o usuário do seu computador."
+        echo -e "${cor_vermelha}ERRO:${reset} O usuário informado não existe. \nPor favor, execute o script novamente e informe o usuário do seu computador."
         exit 1
     fi
 }
